df110ee306aa110d31ddad0294b9346f00c13c54
André Maree <ammaree@users.noreply.github.com>
2025-03-27 11:36:38 +0200

    Initial commit basic working version

---
+++ digole.c
@@ -0,0 +1,143 @@
+// digole.c - Copyright (c) 2017-25 Andre M. Maree / KSS Technologies (Pty) Ltd.
+
+#include "hal_platform.h"
+
+#if (HAL_DIGOLE > 0)
+
+#include "digole.h"
+#include "errors_events.h"
+
+#include <string.h>
+
+// ########################################## MACROS ###############################################
+
+#define	debugFLAG					0xF000
+#define	debugTIMING					(debugFLAG_GLOBAL & debugFLAG & 0x1000)
+#define	debugTRACK					(debugFLAG_GLOBAL & debugFLAG & 0x2000)
+#define	debugPARAM					(debugFLAG_GLOBAL & debugFLAG & 0x4000)
+#define	debugRESULT					(debugFLAG_GLOBAL & debugFLAG & 0x8000)
+
+// ######################################## Enumerations ###########################################
+
+// ######################################### Structures ############################################
+
+// ######################################### Local variables #######################################
+
+digole_t sDigole = { 0 };
+
+// ####################################### Local functions #########################################
+
+static int digoleWriteLen(const char * pccStr, int Len) {
+	return halI2C_Queue(sDigole.psI2C, i2cW, (u8_t *)pccStr, Len, (u8_t *) NULL, 0, (i2cq_p1_t) NULL, (i2cq_p2_t) NULL);
+}
+
+static int digoleWrite(const char * pccStr) { return digoleWriteLen(pccStr, strlen(pccStr)); }
+
+static int digoleWriteText(const char * pccStr) {
+	int iRV = digoleWriteLen("TT", 2);
+	if (iRV > erFAILURE)
+		iRV = digoleWriteLen(pccStr, strlen(pccStr) + 1);
+	return iRV;
+}
+
+static int digoleStoreValue(char * pcBuf, int Val) {
+	int iRV = 0;
+	if (Val > 255) {
+		*pcBuf++ = 255;
+		Val -= 255;
+		++iRV;
+	}
+	*pcBuf++ = Val;
+	return ++iRV;
+}
+
+static int digoleWriteValue(const char * pccStr, int Value) {
+	char caBuf[16];
+	char * pTmp = stpcpy(caBuf, pccStr);				// store command string
+	pTmp += digoleStoreValue(pTmp, Value);				// store value
+	IF_myASSERT(debugTRACK, (pTmp - caBuf) < sizeof(caBuf));
+	return digoleWriteLen(caBuf, pTmp - caBuf);
+}
+
+static int digoleWriteINT(const char * pccStr, int Count, va_list * pvaList) {
+	char caBuf[16];
+	char * pTmp;
+	pTmp = stpcpy(caBuf, pccStr);
+	while (Count--) {
+		pTmp += digoleStoreValue(pTmp, va_arg(*pvaList, int));	// store parameter
+		IF_myASSERT(debugTRACK, (pTmp - caBuf) < sizeof(caBuf));
+	}
+	return digoleWriteLen(caBuf, pTmp - caBuf);
+}
+
+// ###################################### Global functions #########################################
+
+/*
+"\e\034\e\030\003\003\e\001Hello World");		ESC code does not work....
+"FS\000" "FS\121"								only from v2.8
+"DSS\001" "DC\001"								Enable startup and config display
+#define	BASIC	"CLCS\000"												// clear, no cursor
+#define FONT0	"SF\000TT12345678901234567890\000"						// font 0 W=20
+#define FONT6	"SF\006TT1234567890123456789012345678901234567890\000"	// font 6 W=40
+#define FONT10	"SF\012TT12345678901234567890123456\000"				// font 10 W=26
+#define FONT18	"SF\022TT12345678901234567\000"							// font 18 W=17
+#define FONT51	"SF\063TT12345678901\000"								// font 51 W=11
+#define FONT120	"SF\170TT1234567890\000"								// font 120 W=10
+#define FONT123	"SF\173TT12345\000"										// font 123 W=5
+*/
+
+int digoleFunction(digole_e Func, ...) {
+	IF_myASSERT(debugPARAM, Func < digoleNUM);
+	int iRV = erSUCCESS;
+	va_list vaList;
+	va_start(vaList, Func);
+	if (INRANGE(digoleTP, Func, digoleTPTT)) {			// cursor locate and text display (1/2/3 params)
+		if (Func == digoleTP || Func == digoleTPTT)
+			iRV = digoleWriteINT("TP", 2, &vaList);
+		if (iRV > erFAILURE && (Func == digoleTT || Func == digoleTPTT))
+			iRV = digoleWriteText(va_arg(vaList, char *));
+
+	} else if (INRANGE(digoleBL, Func, digoleSF)) {		// backlight/cursor/font selection (1 param)
+		iRV = digoleWriteValue((Func == digoleBL) ? "BL" : (Func == digoleCS) ? "CS" : "SF", va_arg(vaList, int));
+
+	} else if (Func == digoleCLR) {						// clear screen (0 param)
+		digoleWrite("CL");
+
+	} else if (Func == digoleCFG) {						// fixed multiple commands in 1
+		#define digoleCONFIG "CLCS\000SF\022BL\001"
+		iRV = digoleWriteLen(digoleCONFIG, sizeof(digoleCONFIG)-1);
+	}
+	va_end(vaList);
+	return iRV;
+}
+
+int digoleIdentify(i2c_di_t * psI2C) {
+	sDigole.psI2C = psI2C;
+	psI2C->Type = i2cDEV_DIGOLE;
+	psI2C->Speed = i2cSPEED_400;
+	psI2C->TObus = 25;
+	// Must add code to properly identify the device
+	psI2C->IDok = 1;
+	return erSUCCESS;
+}
+
+int digoleConfig(i2c_di_t * psI2C) {
+	int iRV = erINV_STATE;
+	if (psI2C->IDok == 0)
+		goto exit;
+	psI2C->CFGok = 0;
+	halEventUpdateDevice(devMASK_DIGOLE, 0);
+	iRV = digoleSetup();
+	if (iRV < erSUCCESS)
+		goto exit;
+	psI2C->CFGok = 1;
+	halEventUpdateDevice(devMASK_DIGOLE, 1);
+exit:
+	return iRV;
+}
+
+int digoleReport(report_t * psR) {
+	return halI2C_DeviceReport(psR, (void *) sDigole.psI2C);
+}
+
+#endif
---
+++ digole.h
@@ -0,0 +1,104 @@
+// digole.h
+
+#pragma once
+
+#include "hal_i2c_common.h"
+#include "report.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ########################################### Macros ##############################################
+
+// ######################################## Enumerations ###########################################
+
+typedef enum {
+	digoleCFG,						// Configure
+	digoleCLR,						// Clear Screen
+	digoleBL,						// BackLighht <0/1>
+	digoleCS,						// Cursor <0/1>
+	digoleSF,						// Set Font <number>
+	digoleTP,						// Text Position <row,col>
+	digoleTT,						// Text Type <message>
+	digoleTPTT,						// Text Position & Text <row, col, message>
+	digoleNUM,
+} digole_e;
+
+// ######################################### Structures ############################################
+
+typedef struct __attribute__((packed)) digole_t {
+	i2c_di_t *	psI2C;									// size = 4
+} digole_t;
+//DUMB_STATIC_ASSERT(sizeof(digole_t) == 13);
+
+// ###################################### Public variables #########################################
+
+// ###################################### Public functions #########################################
+
+int digoleIdentify(i2c_di_t * psI2C);
+
+int digoleConfig(i2c_di_t * psI2C);
+
+int digoleFunction(digole_e Func, ...);
+
+/**
+ * @brief
+ * @return
+ */
+#define digoleSetup()							digoleFunction(digoleCFG)
+
+/**
+ * @brief
+ * @return
+ */
+#define digoleClear()							digoleFunction(digoleCLR)
+
+/**
+ * @brief
+ * @param[in]
+ * @return
+ */
+#define digoleCursor(OffOn)						digoleFunction(digoleCS, OffOn)
+
+/**
+ * @brief
+ * @param[in]
+ * @return
+ */
+#define digoleSetFont(Font)						digoleFunction(digoleSF, Font)
+
+/**
+ * @brief
+ * @param[in]
+ * @param[in]
+ * @return
+ */
+#define digoleLocate(Row, Col)					digoleFunction(digoleTP, Row, Col)	
+
+/**
+ * @brief
+ * @param[in]
+ * @param[in]
+ * @param[in]
+ * @return
+ */
+#define digolePrintAt(Row, Col, pccStr)			digoleFunction(digoleTPTT, Row, Col, pccStr)	
+
+/**
+ * @brief
+ * @param[in]
+ * @return
+ */
+#define digolePrint(pccStr)						digoleFunction(digoleTT, pccStr)
+
+/**
+ * @brief
+ * @param[in]
+ * @return
+ */
+int digoleReport(struct report_t * psR);
+
+#ifdef __cplusplus
+}
+#endif
